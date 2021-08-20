408fcae97e022c9ffc7fe77d030d68f0e319a916
supervons <supervons@sina.com>
2021-08-20 15:34:28 +0800

    Pugin demo:Use mini-css-extract-plugin to extract css style, create new css file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 // 压缩选项
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// 插件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   // JavaScript 执行入口文件
   entry: './main.js',
@@ -10,6 +12,10 @@ module.exports = {
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist'),
   },
+  plugins: [new MiniCssExtractPlugin({
+    // 文件名称随机
+    filename: `[name]_[contenthash:8].css`,
+  })],
   optimization: {
     minimizer: [new UglifyJsPlugin()],
   },
@@ -18,7 +24,7 @@ module.exports = {
       {
         // 用正则去匹配要用该 loader 转换的 CSS 文件
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader,'css-loader']
       }
     ]
   }
