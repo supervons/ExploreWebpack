be1b50bb777ef1943f9adc3ec8cd0e65e5f8d142
supervons <supervons@sina.com>
2022-09-28 23:51:30 +0800

    Loader inline type.

--- main2.js
+++ main2.js
@@ -1,7 +1,7 @@
 // main2
 require('./main.scss');
-// ./assets 被映射成 images 
-require('images/question.png');
+// ./assets 被映射成 images
+require('!file-loader!images/question.png'); // 内联loader方式加载非JS/JSON资源
 import a from 'question.png' // 能直接引入是因为 webpack 配置了 modules
 // 通过 CommonJS 规范导入 show 函数，不用带 .js 后缀是因为 webpack 的 resolve 里面配置了 extensions
 const show = require('./show');
