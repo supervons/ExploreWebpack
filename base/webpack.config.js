6d17d8d62f7f813bb5cc468be65ecd8767b3c983
supervons <supervons@sina.com>
2023-07-26 13:57:45 +0800

    Base code prettier style.

--- base/webpack.config.js
+++ base/webpack.config.js
@@ -1,48 +1,48 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");// 运行时加载远程应用
-const { ModuleFederationPlugin } = require('webpack').container;
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExternalTemplateRemotesPlugin = require("external-remotes-plugin"); // 运行时加载远程应用
+const { ModuleFederationPlugin } = require("webpack").container;
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
-    entry: './src/main.js',
-    mode: 'development',
-    devServer: {
-        port: 8001,
-        proxy:{
-            "/api":"http://xxx:8088"
-        }
+  entry: "./src/main.js",
+  mode: "development",
+  devServer: {
+    port: 8001,
+    proxy: {
+      "/api": "http://xxx:8088",
     },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: "babel-loader",
-                exclude: /node_modules/,
-                options: {
-                    presets: ["@babel/preset-react"],
-                },
-            },
-            {
-                test: /\.vue$/,
-                use: 'vue-loader',
-            },
-        ],
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'base',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './utils': './src/utils.js'
-            },
-            remotes: {
-                app4: "app4@http://localhost:8002/remoteEntry.js",
-                vue1: "vue1@http://localhost:8003/remoteEntry.js",
-            },
-            shared: { 'react': { singleton: true }, 'react-dom': { singleton: true } },
-        }),
-        new ExternalTemplateRemotesPlugin(),
-        new HtmlWebpackPlugin({template: "./index.html"}),
-        new VueLoaderPlugin()
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
     ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "base",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./utils": "./src/utils.js",
+      },
+      remotes: {
+        app4: "app4@http://localhost:8002/remoteEntry.js",
+        vue1: "vue1@http://localhost:8003/remoteEntry.js",
+      },
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+    new ExternalTemplateRemotesPlugin(),
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new VueLoaderPlugin(),
+  ],
 };
