230a6c4c2ec5f4649684a84e9ba2b51e52d84d32
supervons <supervons@sina.com>
2023-07-27 14:10:37 +0800

    Add exception code.

--- app4/src/app4.js
+++ app4/src/app4.js
@@ -2,9 +2,13 @@ import React, { useEffect } from "react";
 import { getProfile, toCamelCase } from "base/utils";
 export default function app4() {
   useEffect(() => {
-    getProfile().then((res) => {
-      alert("React call:" + JSON.stringify(res));
-    });
+    getProfile()
+      .then((res) => {
+        alert("React call:" + JSON.stringify(res));
+      })
+      .catch((res) => {
+        console.log("err：" + JSON.stringify(res));
+      });
     console.log(toCamelCase("ni hao vons"));
   }, []);
   return <div>hello, react-mfp</div>;
