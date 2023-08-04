a6d21ec9ce8b06ca6bb696c64fbd69287bafff86
supervons <supervons@sina.com>
2023-08-04 14:51:26 +0800

    Base add loader config.

--- base/webpack.config.js
+++ base/webpack.config.js
@@ -26,6 +26,18 @@ module.exports = {
         test: /\.vue$/,
         use: "vue-loader",
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: "url-loader",
+          },
+        ],
+      },
     ],
   },
   plugins: [
