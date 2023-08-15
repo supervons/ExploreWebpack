fd55e2ccc4a6f820114a29325dce60cd5ba2141c
supervons <supervons@sina.com>
2023-08-15 23:19:40 +0800

    Add file-loader

--- vue/webpack.config.js
+++ vue/webpack.config.js
@@ -1,12 +1,12 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExternalTemplateRemotesPlugin = require("external-remotes-plugin"); // 运行时加载远程应用
-const { ModuleFederationPlugin } = require("webpack").container;
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
-let whiteListedModules = ["vue", "element-ui"];
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExternalTemplateRemotesPlugin = require('external-remotes-plugin'); // 运行时加载远程应用
+const { ModuleFederationPlugin } = require('webpack').container;
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+let whiteListedModules = ['vue', 'element-ui'];
 
 module.exports = {
-  entry: "./src/main.js",
-  mode: "development",
+  entry: './src/main.js',
+  mode: 'development',
   devServer: {
     port: 8003,
   },
@@ -14,39 +14,43 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        use: "vue-loader",
+        use: 'vue-loader',
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(ttf|eot|woff|woff2)$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
           },
         ],
       },
       {
         test: /.(jpe?g|png|gif|svg)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
+      },
+      {
+        test: /\.mp4$/,
+        use: 'file-loader?name=videos/[name].[ext]',
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "vue1",
-      filename: "remoteEntry.js",
+      name: 'vue1',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./vue-home": "./src/app.vue",
+        './vue-home': './src/app.vue',
       },
       remotes: {
-        base: "base@http://localhost:8001/remoteEntry.js",
+        base: 'base@http://localhost:8001/remoteEntry.js',
       },
     }),
     new ExternalTemplateRemotesPlugin(),
-    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new HtmlWebpackPlugin({ template: './index.html' }),
     new VueLoaderPlugin(),
   ],
 };
