0bee9798aa859f2546436b968ee3d70cb8957361
supervons <supervons@sina.com>
2023-08-23 13:41:03 +0800

    Add ES6 module export demo.

--- app4/src/app4.js
+++ app4/src/app4.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { getProfile, toCamelCase } from 'base/utils';
+import COMMON_VALUE from 'base/utils';
 export default function app4() {
   useEffect(() => {
+    console.log(COMMON_VALUE);
     getProfile()
       .then(res => {
         console.log('React call:' + JSON.stringify(res));
--- base/src/utils.js
+++ base/src/utils.js
@@ -1,14 +1,14 @@
-import _ from "lodash";
+import _ from 'lodash';
 export function getProfile() {
   return new Promise((resolve, reject) => {
-    fetch("/api/v1/profile/test")
-      .then((res) => {
+    fetch('/api/v1/profile/test')
+      .then(res => {
         return res.json();
       })
-      .then((res) => {
+      .then(res => {
         resolve(res);
       })
-      .catch((err) => {
+      .catch(err => {
         reject(err);
       });
   });
@@ -17,3 +17,6 @@ export function getProfile() {
 export function toCamelCase(str) {
   return _.camelCase(str);
 }
+
+const COMMON_VALUE = 1;
+export default COMMON_VALUE;
